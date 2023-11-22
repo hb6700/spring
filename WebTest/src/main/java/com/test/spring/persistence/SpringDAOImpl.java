f31376cbb4c24cdeca43ddc6445110debe3cf6af
hb6700 <hb6700@naver.com>
2023-11-22 21:39:20 +0900

    231122_update

--- WebTest/src/main/java/com/test/spring/persistence/SpringDAOImpl.java
+++ WebTest/src/main/java/com/test/spring/persistence/SpringDAOImpl.java
@@ -1,11 +1,22 @@
 package com.test.spring.persistence;
 
+import com.test.spring.domain.SpringDTO;
+
 public class SpringDAOImpl implements SpringDAO{
 
 	@Override
 	public int getCount() {
 		
 		return 100;			//select...
+	}
+
+	@Override
+	public int add(SpringDTO dto) {
+
+		//DB작업  > insert
+		System.out.println("insert finish");
+		System.out.println(dto);
+		return 0;
 	}
 
 }
