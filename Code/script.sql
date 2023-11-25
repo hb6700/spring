faec444580d7fc07adf93388cbfed4ba010c8913
hb6700 <hb6700@naver.com>
2023-11-25 23:41:07 +0900

    231124_code

---
+++ Code/script.sql
@@ -0,0 +1,15 @@
+--code > script.sql
+
+create table tblCode(
+    seq number primary key,
+    subject varchar2(100) not null,
+    code varchar2(2000) not null,
+    regdate date default sysdate not null,
+    language varchar2(100) not null
+);
+
+create sequence seqCode;
+
+commit;
+
+select * from tblCode;
