0bc35fb7ce634115e57197d509eb31c373fb6c62
hb6700 <hb6700@naver.com>
2023-12-04 17:54:37 +0900

    231204_Security

---
+++ SecurityTest/src/main/java/com/test/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+	
+	//권한이 없는 페이지를 접근하면 대신 보여줄 페이지
+	@GetMapping(value = "/auto/accesserror.do")
+	public String accesserror(Model model) {
+		return "auto/accesserror";
+	}
+	
+	@GetMapping(value = "/auth/mylogin.do")
+	public String mylogin(Model model) {
+		return "auth/mylogin";
+	}
+	
+	@GetMapping(value = "/auth/mylogout.do")
+	public String mylogout(Model model) {
+		return "auth/mylogout";
+	}
+	
+}
